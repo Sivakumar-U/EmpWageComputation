ad367ce8e3697e42e96db24e644b095da7204a9a
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-03 21:59:45 +0530

    [Siva]Added:Employee part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -x
 
-echo "Welcome to empWageComputation"
-
-isPresent=1
+isFullTime=1
+isPartTime=2
 empRatePerHr=20
-randomCheck=$(( RANDOM%2 ))
+randomCheck=$(( RANDOM%3 ))
 
-if [ $isPresent -eq $randomCheck ]
+if [ $isFullTime -eq $randomCheck ]
 then
       empHrs=8
+elif [ $isPartTime -eq $randomCheck ]
+then
+      empHrs=4
 else
       empHrs=0
 fi
